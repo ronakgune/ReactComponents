18d79e7726f49b34041c396d71cd47a430b3db78
Ronak Gune <ronakgune@gmail.com>
2019-06-27 23:40:23 -0700

    Final
    All

---
+++ components/MyInfo.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Ronak Gune</h1>
+      <p>
+        I am a recent graduate from SCU and am looking for new opportunities.
+      </p>
+      <ol>
+        <li>Spain</li>
+        <li>France</li>
+        <li>Indonesia</li>
+      </ol>
+    </div>
+  );
+}
+
+export default MyInfo;
